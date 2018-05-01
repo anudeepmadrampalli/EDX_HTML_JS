aba8081a70b187cee9bd9d3dbf69a37b5f723103
anudeep571 <anudeep571@gmail.com>
2018-05-01 23:32:45 +0530

    chatbot initial setup

--- chatbot.js
+++ chatbot.js
@@ -1,8 +1,19 @@
 var builder = require('botbuilder');
+var restify = require('restify');
 
-var connector = new builder.ConsoleConnector().listen();
+//Creating the server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function(){
+    console.log("Server %s listening at %s",server.name,server.url);
+});
+
+var connector = new builder.ChatConnector({
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword
+});
+
+server.post('/api/messages',connector.listen());
+
 var bot = new builder.UniversalBot(connector,function(session){
-
     session.send("you said %s",session.message.text);
-
 });
